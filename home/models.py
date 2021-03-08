ee7d33c2733b4c256526c99e77cbd4033040c0ca
Sahil Singh <sahilgsingh55.gs@gmail.com>
2021-03-08 14:18:39 +0530

    added screenshots

--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=122)
-    #father = models.CharField(max_length=122, default="")
+    #father = models.CharField(max_length=122)
     #name2 = models.CharField(max_length=122)
     email = models.CharField(max_length=122)
     phone = models.CharField(max_length=12)
